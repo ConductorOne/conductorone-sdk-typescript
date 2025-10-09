8540cf31aa587a86ea548caaea02f5600b3b428e
Luisina Santos <70584186+luisina-santos@users.noreply.github.com>
2025-10-09 17:48:37 -0300

    fix eslint config (#29)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,8 @@ export default [
       // Handled by typescript compiler
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-namespace": "off",
       "@typescript-eslint/no-unused-expressions": "off",
       "no-unused-private-class-members": "off",
     },
